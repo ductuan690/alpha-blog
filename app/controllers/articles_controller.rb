6387c91897084ac2f83b8d2a7db10d367f3928ea
TuanNgo <ductuan460@gmail.com>
2016-03-30 03:28:33 +0000

    Add show, edit, delete, listing the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,30 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
-  
+
   def show
     @article = Article.find_by(id: params[:id])
+
+    if @article.nil?
+      flash[:notice] = "This article is not found"
+      redirect_to articles_path
+    end
   end
-  
+
+  def edit
+    @article = Article.find_by(id: params[:id])
+
+    if @article.nil?
+      flash[:notice] = "This article is not found"
+      redirect_to articles_path
+    end
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -16,6 +34,25 @@ class ArticlesController < ApplicationController
     else
       render action: :new
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      flash[:notice] = "The article was successfully edited"
+      redirect_to article_path(@article)
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash[:notice] = "The artiicle was deleted"
+    redirect_to articles_path
   end
 
   private
