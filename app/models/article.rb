db065b57b56d97ad48e21de2ee9bf4b6b7acc727
TuanNgo <ductuan460@gmail.com>
2016-03-31 13:58:47 +0000

    Design for showing the article template

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   validates :title, :description, presence: true
   validates :title, length: { minimum: 3, maximum: 50 }
-  validates :description, length: { minimum: 10, maximum: 200 }
+  validates :description, length: { minimum: 10, maximum: 1000 }
 end
