0a57d4f01096c143b1874e4b4c9362a51c6a89ae
TuanNgo <ductuan460@gmail.com>
2016-04-01 08:58:08 +0000

    Add users table and model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
+  
   validates :title, :description, presence: true
   validates :title, length: { minimum: 3, maximum: 50 }
   validates :description, length: { minimum: 10, maximum: 1000 }
