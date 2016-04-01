cf758166c968c79936b7ad78287e231a20a15dbb
TuanNgo <ductuan460@gmail.com>
2016-04-01 09:29:18 +0000

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   has_many :comments
+
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 6, maximum: 20 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            uniqueness: true,
+            length: { maximum: 250 },
+            format: { with: VALID_EMAIL_REGEX }
 end
